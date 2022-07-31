7a1908ff3fbb20c1815148acc26924a0c381685a
Sai Hein Htet Win <saiheinhtet.win@kbzbank.com>
2022-07-31 20:55:27 +0630

    put some code

--- abc.js
+++ abc.js
@@ -1,1 +1,30 @@
-g;
+function question10(x) {
+  var hr = 0;
+  var min = 0;
+  var sec = 0;
+  if (x <= 59) {
+    sec = x;
+    console.log(x + " sec");
+  } else if (x <= 3540) {
+    min = Math.trunc(x / 60);
+    sec = x % 60;
+    if (sec == 0) {
+      console.log(min + " min");
+    } else console.log(min + " min " + sec + " sec");
+  } else {
+    hr = Math.trunc(x / 3600);
+    min = Math.trunc((x % 3600) / 60);
+    sec = (x % 3600) % 60;
+    if (x % 3600 == 0) {
+      console.log(hr + " hr");
+    } else if (min == 0) {
+      console.log(hr + " hr " + sec + " sec");
+    } else if (sec == 0) {
+      console.log(hr + " hr " + min + " min");
+    } else if (hr == 0) {
+      console.log(min + " min " + sec + " sec");
+    } else console.log(hr + " hr " + min + " min " + sec + " sec");
+  }
+}
+
+question10(4568);
